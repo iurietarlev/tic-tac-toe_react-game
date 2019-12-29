98a1bcfdfa6602b4935a5e308206de22ab5b98b4
osk849 <iura.tarlev@gmail.com>
2019-12-29 12:59:50 +0000

    Updates

---
+++ precache-manifest.0befafd5e157f8276525d1762b497c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b286c461fdca2489e0108b090688ff6c",
+    "url": "/tic-tac-toe_react-game/index.html"
+  },
+  {
+    "revision": "4510c109e7a727058099",
+    "url": "/tic-tac-toe_react-game/static/css/main.b52211d8.chunk.css"
+  },
+  {
+    "revision": "d89ee6acc5876b60ae44",
+    "url": "/tic-tac-toe_react-game/static/js/2.99114f26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe_react-game/static/js/2.99114f26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4510c109e7a727058099",
+    "url": "/tic-tac-toe_react-game/static/js/main.9a866943.chunk.js"
+  },
+  {
+    "revision": "b805d55ab74e26fd5071",
+    "url": "/tic-tac-toe_react-game/static/js/runtime-main.14aa4d8c.js"
+  }
+]);
